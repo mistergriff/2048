ec6b79f70646826f86c005e19cdf9fa4b837c4a2
mistergriff <g_renaud@hotmail.ch>
2023-11-06 14:23:06 +0100

    Add score

--- 2048/Program.cs
+++ 2048/Program.cs
@@ -8,30 +8,57 @@ namespace _2048
         {
             const int row = 4;
             const int col = 4;
-
+            int score = 0;
+            int add = 0;
             int[,] grille = new int[row, col];
-
-            // Remplir la grille avec des valeurs (pour l'exemple, des nombres aléatoires)
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    grille[i, j] = 0; // Remplacez ceci par votre propre logique de remplissage
-                }
-            }
-
-            // Afficher la grille
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    Console.Write(grille[i, j] + "\t");
-                }
-                Console.WriteLine(); // Passer à la ligne pour la prochaine rangée
-            }
+            
+            FillGrid();
+            ShowScreen();
 
             Console.ReadKey();
-        }
 
+          
+
+            //Fonction de remplissage de la grille
+            void FillGrid()
+            {
+                // Remplir la grille avec des valeurs
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        grille[i, j] = 0;
+                    }
+                }
+            }
+
+
+            //Fonction d'affichage de la grille
+            void ShowScreen()
+            {
+                //Afficher le score
+                Console.WriteLine("Score :  {0}\n", AddScore(add));
+
+                // Afficher la grille
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        Console.Write(grille[i, j] + "\t");
+                    }
+                    Console.WriteLine(); // Passer à la ligne pour la prochaine rangée
+                }
+            }
+
+
+            //Fonction de calcul du score
+            int AddScore(int addScore)
+            {
+                score += addScore;
+                return score;
+            }
+
+
+        }
     }
 }
