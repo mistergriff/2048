e0c7dbbffe9c272a0580897d7a1cb169720fd0e6
mistergriff <g_renaud@hotmail.ch>
2023-11-06 11:21:54 +0100

    update grid

--- 2048/Program.cs
+++ 2048/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2048
 {
@@ -10,9 +6,32 @@ namespace _2048
     {
         static void Main(string[] args)
         {
+            const int row = 4;
+            const int col = 4;
 
+            int[,] grille = new int[row, col];
 
+            // Remplir la grille avec des valeurs (pour l'exemple, des nombres aléatoires)
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    grille[i, j] = 0; // Remplacez ceci par votre propre logique de remplissage
+                }
+            }
 
+            // Afficher la grille
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(grille[i, j] + "\t");
+                }
+                Console.WriteLine(); // Passer à la ligne pour la prochaine rangée
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
